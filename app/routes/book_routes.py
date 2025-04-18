ace1e9dbe826e37304ba233041d71b014ce91118
Jeslyn Lai <imlyj5@gmail.com>
2025-04-18 09:29:26 -0700

    400 and 404 handling with helper function

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from app.models.book import books
+from flask import Blueprint, abort, make_response 
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
@@ -19,10 +20,25 @@ def get_all_books():
 
 @books_bp.get("/<book_id>")
 def get_one_book(book_id):
+    book = validate_book(book_id)
+
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description,
+    }
+
+    
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        response = {"message": f"book {book_id} invalid"}
+        abort(make_response(response, 400))
+
     for book in books:
-        if book.id == int(book_id):
-            return {
-                    "id":book.id,
-                    "title":book.title,
-                    "description":book.description
-                }
+        if book.id == book_id:
+            return book
+
+    response = {"message": f"book {book_id} not found"}
+    abort(make_response(response, 404))
